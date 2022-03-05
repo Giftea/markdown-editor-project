fcadcfdb3e7be66a2371f916e91ee2f040b9f6ce
Uhiene Gift Eleojo <eleojogiftuhiene@gmail.com>
2022-03-05 22:22:21 +0100

    project initialization

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className={styles.header}>Markdown Project</h1>
+        </div>
+    )
+}
+
+export default Header
